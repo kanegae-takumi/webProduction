ca47a6fe3ad1fd47ae4b1cb847528709f3354893
sasahara08 <yukio.sasahara.08@gmail.com>
2025-03-11 11:54:42 +0900

    Bean追加(笹原)

--- src/main/java/dto/GameBean.java
+++ src/main/java/dto/GameBean.java
@@ -1,5 +1,90 @@
 package dto;
 
-public class GameBean {
+import java.io.Serializable;
+import java.sql.Time;
+import java.util.Date;
+
+public class GameBean implements Serializable {
+
+	private int gameId;
+	private Date gameDate;
+	private Time startTime;
+	private int homeTeamId;
+	private int awayTeamId;
+	private int stadiumId;
+	private Date gameDeleteAt;
+	
+	public GameBean(int gameId, Date gameDate, Time startTime, int homeTeamId, int awayTeamId, int stadiumId,
+			Date gameDeleteAt) {
+		this.gameId = gameId;
+		this.gameDate = gameDate;
+		this.startTime = startTime;
+		this.homeTeamId = homeTeamId;
+		this.awayTeamId = awayTeamId;
+		this.stadiumId = stadiumId;
+		this.gameDeleteAt = gameDeleteAt;
+	}
+
+	public int getGameId() {
+		return gameId;
+	}
+
+	public void setGameId(int gameId) {
+		this.gameId = gameId;
+	}
+
+	public Date getGameDate() {
+		return gameDate;
+	}
+
+	public void setGameDate(Date gameDate) {
+		this.gameDate = gameDate;
+	}
+
+	public Time getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Time startTime) {
+		this.startTime = startTime;
+	}
+
+	public int getHomeTeamId() {
+		return homeTeamId;
+	}
+
+	public void setHomeTeamId(int homeTeamId) {
+		this.homeTeamId = homeTeamId;
+	}
+
+	public int getAwayTeamId() {
+		return awayTeamId;
+	}
+
+	public void setAwayTeamId(int awayTeamId) {
+		this.awayTeamId = awayTeamId;
+	}
+
+	public int getStadiumId() {
+		return stadiumId;
+	}
+
+	public void setStadiumId(int stadiumId) {
+		this.stadiumId = stadiumId;
+	}
+
+	public Date getGameDeleteAt() {
+		return gameDeleteAt;
+	}
+
+	public void setGameDeleteAt(Date gameDeleteAt) {
+		this.gameDeleteAt = gameDeleteAt;
+	}
+	
+	
+	
+	
+
+	
 
 }
